c434bfbc40a5dd14ba665b1e2fa6175cbdca169d
Honffy Daniel <honffy.daniel@gmail.com>
2019-09-26 09:25:15 +0200

    add addRestaurant to the controller, rename button froma save to add

--- FoodService/FoodService/Controllers/RestaurantController.cs
+++ FoodService/FoodService/Controllers/RestaurantController.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodService.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodService.Controllers
 {
     public class RestaurantController : Controller
     {
+
+        private readonly IAuthorService authorService;
+
+        [HttpPost("/AddRestaurant")]
+        public IActionResult AddRestaurant(Restaurant newRestaurant)
+        {
+            //itt kéne valahogy az adatbázisba egy új ID-vel berakni
+            return View();
+        }
+
+        [HttpGet("/home")]
         public IActionResult Index()
         {
             return View();
