4a3a263428bcc96ad9b580faf90b81b3f29582b6
Honffy Daniel <honffy.daniel@gmail.com>
2019-10-01 13:25:21 +0200

    Add SearchRestaurantViewModel

---
+++ FoodService/FoodService/Models/ViewModels/Restaurant/SearchRestaurantViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodService.Models.ViewModels.Restaurant
+{
+    public class SearchRestaurantViewModel
+    {
+    }
+}
