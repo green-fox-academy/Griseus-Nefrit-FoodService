35e6d38b457ffc2eb8cd278d07b38a4a3daf6267
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-10-01 10:08:07 +0200

    ApplicationDbContext change

--- FoodService/FoodService/ApplicationDbContext.cs
+++ FoodService/FoodService/ApplicationDbContext.cs
@@ -1,21 +1,33 @@
-﻿using JetBrains.Annotations;
+﻿using FoodService.Models;
+using FoodService.Models.Identity;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using FoodService.Models;
 
 namespace FoodService
 {
-  public class ApplicationDbContext : DbContext
-  {
-    public DbSet<Meal> Meals { get; set; }
-    public DbSet<Restaurant> Restaurants { get; set; }
-    
-    public DbSet<Price> Prices { get; set; }
-    public ApplicationDbContext(DbContextOptions options) : base(options)
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<Price> Prices { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole { Name = "Admin", NormalizedName = "Admin".ToUpper() },
+                new IdentityRole { Name = "Manager", NormalizedName = "Manager".ToUpper() },
+                new IdentityRole { Name = "Customer", NormalizedName = "Customer".ToUpper() });
+        }
     }
-  }
 }
