f07203e22429f6097c9f9db99430516a178bad7e
Honffy Daniel <honffy.daniel@gmail.com>
2019-10-02 11:03:10 +0200

    Modify asp-for value

--- FoodService/FoodService/Controllers/HomeController.cs
+++ FoodService/FoodService/Controllers/HomeController.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FoodService.Models;
-using FoodService.Models.RequestModels.Restaurant;
 using FoodService.Services;
 using FoodService.Services.RestaurantService;
 using Microsoft.AspNetCore.Mvc;
 using ReflectionIT.Mvc.Paging;
-using FoodService.Models.ViewModels.Restaurant;
 
 namespace FoodService.Controllers
 {
@@ -22,34 +20,19 @@ namespace FoodService.Controllers
         }
 
         [HttpGet("/")]
-        public async Task<IActionResult> Index(SearchRestaurantRequest searchRestaurantRequest, int page = 1)
+        public async Task<IActionResult> Index(int page = 1)
         {
             List<Restaurant> restaurants;
             if (User.IsInRole("Manager"))
             {
                 restaurants = await restaurantService.FindByManagerNameOrEmailAsync(User.Identity.Name);
             }
-            else if(searchRestaurantRequest.FoodName == "" || searchRestaurantRequest.FoodName == null)
+            else
             {
                 restaurants = await restaurantService.FindAllAsync();
             }
-            else
-            {
-                restaurants = await restaurantService.FindByFoodNameAsync(searchRestaurantRequest.FoodName);
-            }
-
-            SearchRestaurantViewModel searchRestaurantViewModel = new SearchRestaurantViewModel()
-            {
-                PagingList = PagingList.Create(restaurants, 10, page),
-                SearchRestaurantRequest = searchRestaurantRequest
-            };
-            return View(searchRestaurantViewModel);
-        }
-
-        [HttpPost]
-        public IActionResult SearchRestaurantAsync(SearchRestaurantViewModel searchRestaurantViewModel)
-        {
-            return RedirectToAction(nameof(HomeController.Index), "Home", searchRestaurantViewModel.SearchRestaurantRequest.FoodName);
+            var model = PagingList.Create(restaurants, 10, page);
+            return View(model);
         }
     }
 }
