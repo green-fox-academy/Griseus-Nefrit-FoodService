42b3afa15368acc4874311b0202223d52efc7163
Honffy Daniel <honffy.daniel@gmail.com>
2019-10-09 20:49:20 +0200

    Add UserSettingsController

---
+++ FoodService/FoodService/Controllers/UserSettingsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace FoodService.Controllers
+{
+    public class UserSettingsController : Controller
+    {
+        private readonly IStringLocalizer<UserSettingsController> localizer;
+
+        public UserSettingsController(IStringLocalizer<UserSettingsController> localizer)
+        {
+            this.localizer = localizer;
+        }
+
+        [Authorize(Roles = "Customer, Manager, Admin")]
+        [HttpGet]
+        public IActionResult UserSettings()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Customer, Manager, Admin")]
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewData["Message"] = localizer["Description"];
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
