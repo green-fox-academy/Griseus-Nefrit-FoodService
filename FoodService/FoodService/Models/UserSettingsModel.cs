c726ab7f4cace080f110832470eeae6b5300b749
Honffy Daniel <honffy.daniel@gmail.com>
2019-10-09 21:07:55 +0200

    Add userSettingsModel

---
+++ FoodService/FoodService/Models/UserSettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodService.Models
+{
+    public class UserSettings
+    {
+        public RequestCulture Culture { get; set; }
+    }
+}
