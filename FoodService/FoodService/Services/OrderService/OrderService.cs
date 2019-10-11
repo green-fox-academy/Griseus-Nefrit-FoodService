b7fb8c0eda17b8d2980191892bb0fc988a7d2b41
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-10-11 13:52:34 +0200

    Basket deleted from menu and for time UTC used

--- FoodService/FoodService/Services/OrderService/OrderService.cs
+++ FoodService/FoodService/Services/OrderService/OrderService.cs
@@ -72,8 +72,8 @@ namespace FoodService.Services.OrderService
                 {
                     shoppingCartDraft = new Order()
                     {
-                        DateCreated = DateTime.Now,
-                        LastUpdate = DateTime.Now,
+                        DateCreated = DateTime.UtcNow,
+                        LastUpdate = DateTime.UtcNow,
                         User = user,
                         Restaurant = restaurant,
                         OrderStatus = OrderStatus.Draft
