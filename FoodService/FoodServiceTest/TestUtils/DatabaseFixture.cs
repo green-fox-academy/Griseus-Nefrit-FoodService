46a823a92407db28579b23460e025adfd5c6ee6f
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-10-28 10:53:24 +0100

    Add test environment

---
+++ FoodService/FoodServiceTest/TestUtils/DatabaseFixture.cs
@@ -0,0 +1,40 @@
+﻿using FoodService;
+using FoodService.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodServiceTest.TestUtils
+{
+    public class DatabaseFixture : IDisposable
+    {
+        private readonly DbContextOptions<ApplicationDbContext> options;
+        public DatabaseFixture()
+        {
+            options = TestDbOptions.Get();
+            using (var context = new ApplicationDbContext(options))
+            {
+                SeedPosts(context);
+                context.SaveChanges();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Restaurants.RemoveRange(context.Restaurants);
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedPosts(ApplicationDbContext context)
+        {
+            context.Restaurants.AddRange(new List<Restaurant>
+            {
+                new Restaurant { Name = "Test" },
+            });
+        }
+    }
+}
---
+++ FoodService/FoodServiceTest/TestUtils/TestDbOptions.cs
@@ -0,0 +1,19 @@
+﻿using FoodService;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace FoodServiceTest.TestUtils
+{
+    class TestDbOptions
+    {
+        public static DbContextOptions<ApplicationDbContext> Get()
+        {
+            return new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "foodservice-testdb")
+                .Options;
+        }
+    }
+}
